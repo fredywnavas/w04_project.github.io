4a365261f579a5039f3afeee58f9edcfc2efff2d
Laura Nuñez <77087152+Ichlaura@users.noreply.github.com>
2022-03-21 21:12:37 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello my name is Fredy");
+	System.out.println("Hello Fredy, I am Laura");
+
     }
 }
